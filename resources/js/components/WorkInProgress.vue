d74573cdaef94183f7b58acbc7d4b7e49418c9ef
hexeva <patrick79.g@gmail.com>
2022-02-21 18:32:36 +0100

    front-office setup

---
+++ resources/js/components/WorkInProgress.vue
@@ -0,0 +1,11 @@
+<template>
+    <section>
+        LAVORI IN CORSO
+    </section>
+</template>
+
+<script>
+export default {
+    name:'WorkInProgress',
+}
+</script>
---
+++ resources/js/views/App.vue
@@ -0,0 +1,15 @@
+<template>
+    <section>
+       <WorkInProgress />
+    </section>
+</template>
+
+<script>
+import WorkInProgress from '../components/WorkInProgress.vue'
+export default {
+    name:'App',
+    components:{
+        WorkInProgress,
+    }
+}
+</script>
