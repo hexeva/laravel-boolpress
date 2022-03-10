25db9d71430562666700431e470cdfebe232093b
hexeva <patrick79.g@gmail.com>
2022-03-10 08:54:14 +0100

    push

--- app/UserInfo.php
+++ app/UserInfo.php
@@ -8,7 +8,7 @@ class UserInfo extends Model
 {
     protected $table ='user_info';
 
-    // FUNZIONE PER COLLEGARE USER_INFO AL MODEL PRINCIPALE USER
+    // FUNZIONE PER COLLEGARE USER_INFO AL MODEL PRINCIPALE USER (one to one)
     public function user(){
         return $this->belongsTo('App\User');
     }
